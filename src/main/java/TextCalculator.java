ca59c292830b1c5d6470a17db9ee424e151d6807
jerry92k <jens92.k@gmail.com>
2021-12-27 17:48:18 +0900

    feat: 2) 메서드는 정수를 0, 1, 2개 받을 수 있고, 그 합계를 돌려준다. 빈 문자열의 경우 0을 반환한다.

--- src/main/java/TextCalculator.java
+++ src/main/java/TextCalculator.java
@@ -1,6 +1,21 @@
+import java.util.Arrays;
+
 public class TextCalculator {
 
-    public int add(String numbers) {
-        return 0;
+    public int add(String textNumbers) {
+        if(textNumbers.isEmpty()){
+            return 0;
+        }
+        String[] splited = textNumbers.split(",");
+        if(splited.length>2){
+            throw new IllegalArgumentException("정수는 0,1,2 개만 받을 수 있습니다.");
+        }
+        try {
+            return Arrays.stream(splited)
+                .mapToInt(Integer::parseInt)
+                .sum();
+        }catch (NumberFormatException ex){
+            throw new NumberFormatException("입력형식이 잘못되었습니다.");
+        }
     }
 }
--- src/test/java/TextCalculatorTest.java
+++ src/test/java/TextCalculatorTest.java
@@ -1,13 +1,45 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class TextCalculatorTest {
 
+    @DisplayName("빈문자열인 경우 0을 리턴한다.")
     @Test
-    void add(){
-        String numbers="123";
+    void add1() {
+        String textNumbers="";
         TextCalculator textCalculator = new TextCalculator();
-        assertThat(textCalculator.add(numbers)).isEqualTo(0);
+        assertThat(textCalculator.add(textNumbers)).isEqualTo(0);
     }
+
+    @DisplayName("문자열로 1~2개의 정수를 입력받아 계산한다.")
+    @CsvSource(value = {"1:1","1,2:3","0,7:7"},delimiter = ':')
+    @ParameterizedTest
+    void add2(String textNumbers, int expectedSum) {
+        TextCalculator textCalculator = new TextCalculator();
+        assertThat(textCalculator.add(textNumbers)).isEqualTo(expectedSum);
+    }
+
+    @DisplayName("2개 초과인 경우 오류")
+    @CsvSource(value = {"1,2,3:6","0,0,1:1"},delimiter = ':')
+    @ParameterizedTest
+    void add_exception1(String textNumbers, int expectedSum) {
+        TextCalculator textCalculator = new TextCalculator();
+        assertThatThrownBy(()->textCalculator.add(textNumbers))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("정수가 아닌 경우 오류")
+    @CsvSource(value = {"1.0:1","0.1,2:3","하나,7:8"},delimiter = ':')
+    @ParameterizedTest
+    void add_exception2(String textNumbers, int expectedSum) {
+        TextCalculator textCalculator = new TextCalculator();
+        assertThatThrownBy(()->textCalculator.add(textNumbers))
+            .isInstanceOf(NumberFormatException.class);
+    }
+
 }
